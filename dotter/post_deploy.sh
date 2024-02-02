e17b2731f35b6febc911d79ee81dd9bcc54287a2
Richard Beneyton <richard.beneyton@gmail.com>
2024-02-02 01:37:18 +0100

    firefox: better bottom layout
    thanks to specialists, weird buttons effect are solved now.
    
    + add setting to circumvent super tiny bookmark folder browsing.

--- dotter/post_deploy.sh
+++ dotter/post_deploy.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
 SRC=~/.mozilla/firefox/.dotter/chrome/userChrome.css
+INCLUDE=~/.mozilla/firefox/.dotter/chrome/include
 # grab all profiles
 for i in ~/.mozilla/firefox/*/prefs.js
 do
     DIR=$(dirname $i)
     mkdir -p $DIR/chrome
     ln -fs $SRC $DIR/chrome/userChrome.css
+    rm -rf $DIR/chrome/include
+    ln -fs $INCLUDE $DIR/chrome/include
 done
