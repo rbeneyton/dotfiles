20fc12c5b2c399091f46fba20a140754368a9332
Richard Beneyton <richard.beneyton@gmail.com>
2024-12-22 15:46:27 +0100

    nvim: hover setup + rustaceanvim

--- nvim/after/ftplugin/rust.lua
+++ nvim/after/ftplugin/rust.lua
@@ -1,11 +1,14 @@
 local bufnr = vim.api.nvim_get_current_buf()
-vim.keymap.set(
-  "n", 
-  "<leader>a", 
-  function()
-    vim.cmd.RustLsp('codeAction') -- supports rust-analyzer's grouping
-    -- or vim.lsp.buf.codeAction() if you don't want grouping.
-  end,
-  { silent = true, buffer = bufnr }
-)
+
+-- codeAction
+vim.keymap.set("n", "<leader>a", function()
+  vim.cmd.RustLsp("codeAction") -- supports rust-analyzer's grouping
+  -- or vim.lsp.buf.codeAction() if you don't want grouping.
+end, { silent = true, buffer = bufnr })
+
+-- Override Neovim's built-in hover keymap with rustaceanvim's hover actions
+vim.keymap.set("n", "K", function()
+  vim.cmd.RustLsp({ "hover", "actions" })
+end, { silent = true, buffer = bufnr })
+
 vim.lsp.inlay_hint.enable(true, { bufnr = bufnr }) -- always inlay
