46db0a39b0c0f4583e87638059f52e0932237eba
Richard Beneyton <richard.beneyton@gmail.com>
2026-03-30 23:45:23 +0200

    ipython: proper polars setup

--- ipython/startup/60-polars.py
+++ ipython/startup/60-polars.py
@@ -3,11 +3,11 @@ try:
     import polars.selector as cs
 
     # expand up to 40 columns
-    pl.config.set_tbl_cols(40)
+    pl.Config().set_tbl_cols(40)
     # pl.config.set_tbl_width_chars(width) ?
 
     # expand rows until 500
-    pl.config.set_tbl_cols(500)
+    pl.Config().set_tbl_rows(500)
 
 except:
     pass
